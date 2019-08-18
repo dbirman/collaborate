226f7704096ec17d3008e0339f11a567ba7af9d6
Dan Birman <danbirman@gmail.com>
2019-08-17 22:52:56 -0700

    Updating to functional version of contributions

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ function chgBkg(cell) {
 	} else {
 		cell.className = "w";
 	}
+	generateHTML();
 }
+
+function generateHTML() {
+	// Generate the inner html 
+	var temp = "<p>" + document.getElementById("table").innerHTML + "</p>";
+	var temp = temp.replace(/ onclick.*>/g,">");
+	document.getElementById("raw").innerHTML = temp;
+}
+
+window.onload = generateHTML;
